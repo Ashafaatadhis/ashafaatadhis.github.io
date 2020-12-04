6a7e0f2d646c8985f147bf7ca355f23fa93d2e2a
Adhis Mauliyahsa Ashafaat <67637463+Ashafaatadhis@users.noreply.github.com>
2020-12-04 15:56:10 +0700

    Add files via upload

---
+++ ultah/script.js
@@ -0,0 +1,60 @@
+const play = (e) => {
+    e.style.display = 'none';
+    if(innerWidth >= 1028){
+        document.querySelector('.container').style.backgroundImage = "url('ultahBGfull.jpg')";
+        document.querySelector('.container').style.backgroundSize = "cover";
+    }else{
+    document.querySelector('.container').style.backgroundImage = "url('ultahBG.jpg')";
+    document.querySelector('.container').style.backgroundSize = "cover";
+    }
+
+    
+    document.querySelector('.container').innerHTML = `
+        <iframe id="viddeo" src="http://www.YoutubeRepeater.com/watch?v=gsMdSS1qwXE&s=14&autoplay=1"  allow='autoplay' allowfullscreen></iframe>
+        <div class="gambarGif1"></div>
+        <div class="gambarGif2"></div>
+    `;
+
+    document.querySelector('#viddeo').style.display = 'none';
+    document.querySelector('.gambarGif1').style.backgroundImage = "url('gifult1.gif')";
+    document.querySelector('.gambarGif1').style.backgroundSize = "cover";
+    document.querySelector('.gambarGif1').style.backgroundRepeat = "no-repeat";
+    
+    document.querySelector('.gambarGif1').style.position = 'absolute';
+    
+    document.querySelector('.gambarGif2').style.backgroundImage = "url('gifult2.gif')";
+    document.querySelector('.gambarGif2').style.backgroundSize = "cover";
+    document.querySelector('.gambarGif2').style.backgroundRepeat = "no-repeat";
+    
+    document.querySelector('.gambarGif2').style.position = 'absolute';
+
+    if(innerWidth >= 1028){
+        document.querySelector('.gambarGif1').style.top = '400px';
+        document.querySelector('.gambarGif1').style.left = '500px';
+        document.querySelector('.gambarGif1').style.width = '600px';
+        document.querySelector('.gambarGif1').style.height = '500px';
+        
+        document.querySelector('.gambarGif2').style.top = '100px';
+        document.querySelector('.gambarGif2').style.right = '200px';
+        document.querySelector('.gambarGif2').style.width = '600px';
+        document.querySelector('.gambarGif2').style.height = '500px';
+
+    }else{
+        document.querySelector('.gambarGif1').style.top = '650px';
+        document.querySelector('.gambarGif1').style.left = '200px';
+        document.querySelector('.gambarGif1').style.width = '400px';
+        document.querySelector('.gambarGif1').style.height = '300px';
+    
+        document.querySelector('.gambarGif2').style.top = '50px';
+        document.querySelector('.gambarGif2').style.left = '200px';
+        document.querySelector('.gambarGif2').style.width = '400px';
+        document.querySelector('.gambarGif2').style.height = '300px';
+    }
+
+    setTimeout(() => {
+        swal({
+            text: "Hehey not bad... habede Dewi ..zz\n\t -Adhis",
+            button: "Tutup",
+          });
+    }, 3000)
+};
